0e0670aa044bbc94145b363bc00666e715672d63
Ron Rahaman <rrahaman6@gatech.edu>
2022-04-14 16:32:03 -0400

    Fixed walltime in form

--- form.js
+++ form.js
@@ -1,64 +1,76 @@
 'use strict'
 
-/*
- * Function to disable the gpu option initially.
- */
-function disable_num_gpus_input(){
-    let num_gpus_input = $('#num_gpus');
-    num_gpus_input.attr('disabled', 'disabled');
-    $('#num_gpus').val(0);
+let max_gpus = new Map([
+  ["cpu", 0],
+  ["cpu-sas", 0],
+  ["RTX6000", 4],
+  ["TeslaV100-16GB", 2],
+  ["TeslaV100-32GB", 2]
+])
+
+function set_max_time(queue_name, node_type) {
+  let max_hours = 21 * 24;  // Max for inferno CPU
+  if (queue_name == "embers") {
+    max_hours = 8;  // Max for embers
+  }
+  else if (max_gpus.has(node_type) && max_gpus.get(node_type) > 0) {
+    max_hours = 3 * 24;  // Max for inferno GPU
+  }
+
+  let num_hours = $('#bc_num_hours');
+  num_hours.val(Math.min(max_hours, num_hours.val()))
+  num_hours.attr('max', max_hours);
 }
 
-
-
-/*
- *Function to enable the gpu input.
- */
-function enable_num_gpus_input(selected_node_type){
-    let num_gpus_input = $('#num_gpus');
-    if(selected_node_type[0].value === "RTX6000"){
-      num_gpus['max'] = 4;
-    } else {
-      num_gpus['max'] = 2;
-    }
-    num_gpus_input.removeAttr('disabled');
+function set_num_gpus(node_type) {
+  let num_gpus = $('#num_gpus')
+  let max_val = max_gpus.has(node_type) ? max_gpus.get(node_type) : 0;
+  num_gpus.attr('max', max_val);
+  if (max_val > 0) {
+    num_gpus.removeAttr('disabled');
+    let v = num_gpus.val();  // Current value
+    num_gpus.attr('min', 1);
+    num_gpus.val(Math.min(Math.max(v, 1), max_val));
+  }
+  else {
+    num_gpus.attr('min', 0);
+    num_gpus.val(0);
+    num_gpus.attr('disabled', 'disabled');
+  }
 }
-
 
 /*
  * Function to handle the change event in the custom_queue select dropdown
  */
 
-function node_type_change_handler(selected_node_type)
-{
-    let node_type_name = selected_node_type[0].value;
-    let no_gpu_type_1= ["cpu"];
-    let no_gpu_type_2= ["cpusas"];
-    if(no_gpu_type_1.indexOf(node_type_name)>-1 ||
-       no_gpu_type_2.indexOf(node_type_name)>-1 ){
-      disable_num_gpus_input();
-      $('#num_gpus').val(0);
-    }
-    else {
-      enable_num_gpus_input(selected_node_type);
-    }
+function node_type_change_handler(selected_node_type) {
+  let queue_name = $('#custom_queue');
+  let node_type = selected_node_type[0].value;
+  set_max_time(queue_name, node_type);
+  set_num_gpus(node_type);
 };
 
-/*
- * Invoke the functions when the page loads.
- */
-$(document).ready(function(){
+function custom_queue_change_handler(selected_custom_queue) {
+  let queue_name = selected_custom_queue[0].value;
+  let node_type = $('#node_type');
+  set_max_time(queue_name, node_type);
+};
 
-    $('select').find('option[value=cpu]').attr('selected','selected');
+$(document).ready(function () {
 
-    //Initially set the num_gpus to 0 for default cpu node type.
-    $('#num_gpus').val(0);
-    disable_num_gpus_input();
+  $('select').find('option[value=cpu]').attr('selected', 'selected');
 
-    let node_type = $('#batch_connect_session_context_node_type');
+  let node_type = $('#batch_connect_session_context_node_type');
+  let custom_queue = $('#batch_connect_session_context_custom_queue');
 
-    //Handles the change event.
-    node_type.change(function(){
-        node_type_change_handler(node_type);
-    })
+  set_num_gpus(node_type)
+  set_max_time(custom_queue, node_type)
+
+  //Handles the change event.
+  node_type.change(function () {
+    node_type_change_handler(node_type);
+  })
+  custom_queue.change(function () {
+    custom_queue_change_handler(custom_queue);
+  })
 });
