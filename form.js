8b3fe500464c4d36756f61b25d947870d42081a4
Ron Rahaman <rrahaman6@gatech.edu>
2022-09-27 16:14:11 -0400

    WIP: Add QOS and node type

--- form.js
+++ form.js
@@ -1,117 +1,41 @@
+"use strict"
+
 // Can retrieve these using get_resource_limits.py script
 
 let max_nodes = new Map([
-    ['cpu-small', 2],
-    ['cpu-medium', 0],
-    ['cpu-large', 0],
-    ['cpu-sas', 0],
-    ['cpu-amd', 0],
-    ['gpu-v100', 1],
-    ['gpu-a100', 0],
-    ['gpu-rtx6000', 0],
-    ['cpu-pmem', 0],
-    ['cpu-small-X', 2],
-    ['cpu-medium-X', 0],
-    ['cpu-large-X', 0],
-    ['cpu-sas-X', 0],
-    ['cpu-amd-X', 0],
-    ['gpu-v100-X', 1],
-    ['gpu-a100-X', 0],
-    ['gpu-rtx6000-X', 0],
-    ['cpu-pmem-X', 0],
+    ['cpu', 2],
+    ['V100-16GB', 1],
 ])
 
 let max_walltimes = new Map([
-    ['cpu-small', 504],
-    ['cpu-medium', 504],
-    ['cpu-large', 504],
-    ['cpu-sas', 504],
-    ['cpu-amd', 504],
-    ['gpu-v100', 72],
-    ['gpu-a100', 72],
-    ['gpu-rtx6000', 72],
-    ['cpu-pmem', 504],
-    ['cpu-small-X', 504],
-    ['cpu-medium-X', 504],
-    ['cpu-large-X', 504],
-    ['cpu-sas-X', 504],
-    ['cpu-amd-X', 504],
-    ['gpu-v100-X', 72],
-    ['gpu-a100-X', 72],
-    ['gpu-rtx6000-X', 72],
-    ['cpu-pmem-X', 504],
+    ['cpu', 504],
+    ['V100-16GB', 72],
 ])
 
 let max_cores = new Map([
-    ['cpu-small', 24],
-    ['cpu-medium', 0],
-    ['cpu-large', 0],
-    ['cpu-sas', 0],
-    ['cpu-amd', 0],
-    ['gpu-v100', 24],
-    ['gpu-a100', 0],
-    ['gpu-rtx6000', 0],
-    ['cpu-pmem', 0],
-    ['cpu-small-X', 24],
-    ['cpu-medium-X', 0],
-    ['cpu-large-X', 0],
-    ['cpu-sas-X', 0],
-    ['cpu-amd-X', 0],
-    ['gpu-v100-X', 24],
-    ['gpu-a100-X', 0],
-    ['gpu-rtx6000-X', 0],
-    ['cpu-pmem-X', 0],
+    ['cpu', 24],
+    ['V100-16GB', 24],
 ])
 
 let max_node_mem = new Map([
-    ['cpu-small', 191],
-    ['cpu-medium', 0],
-    ['cpu-large', 0],
-    ['cpu-sas', 0],
-    ['cpu-amd', 0],
-    ['gpu-v100', 385],
-    ['gpu-a100', 0],
-    ['gpu-rtx6000', 0],
-    ['cpu-pmem', 0],
-    ['cpu-small-X', 191],
-    ['cpu-medium-X', 0],
-    ['cpu-large-X', 0],
-    ['cpu-sas-X', 0],
-    ['cpu-amd-X', 0],
-    ['gpu-v100-X', 385],
-    ['gpu-a100-X', 0],
-    ['gpu-rtx6000-X', 0],
-    ['cpu-pmem-X', 0],
+    ['cpu', 191],
+    ['V100-16GB', 385],
 ])
 
 let max_gpus = new Map([
-    ['cpu-small', 0],
-    ['cpu-medium', 0],
-    ['cpu-large', 0],
-    ['cpu-sas', 0],
-    ['cpu-amd', 0],
-    ['gpu-v100', 2],
-    ['gpu-a100', 0],
-    ['gpu-rtx6000', 0],
-    ['cpu-pmem', 0],
-    ['cpu-small-X', 0],
-    ['cpu-medium-X', 0],
-    ['cpu-large-X', 0],
-    ['cpu-sas-X', 0],
-    ['cpu-amd-X', 0],
-    ['gpu-v100-X', 2],
-    ['gpu-a100-X', 0],
-    ['gpu-rtx6000-X', 0],
-    ['cpu-pmem-X', 0],
+    ['cpu', 0],
+    ['V100-16GB', 2],
 ])
 
-function _update_gpu_inputs(selected_custom_queue) {
-    let queue_name = selected_custom_queue[0].value
+
+
+function _update_gpu_inputs(selected_node_type) {
+    let node_type = selected_node_type[0].value
     let num_gpus = $('#num_gpus')
 
     //console.log("Queue name:", queue_name)
 
-    let gmax = max_gpus.has(queue_name) ? max_gpus.get(queue_name) : 0
+    let gmax = max_gpus.has(node_type) ? max_gpus.get(node_type) : 0
     //console.log("> Max GPUs: ", gmax)
 
     if (gmax > 0) {
@@ -133,63 +57,63 @@ function _update_gpu_inputs(selected_custom_queue) {
     }
 }
 
-function _update_cores_inputs(selected_custom_queue) {
-    let queue_name = selected_custom_queue[0].value
+function _update_cores_inputs(selected_node_type) {
+    let node_type = selected_node_type[0].value
     let num_cores = $('#num_cores')
 
     //console.log("Queue name:", queue_name)
 
-    if (max_cores.has(queue_name)) {
-        let cmax = max_cores.get(queue_name)
+    if (max_cores.has(node_type)) {
+        let cmax = max_cores.get(node_type)
         //console.log("> Max cores: ", cmax)
         num_cores.attr('max', cmax)
         num_cores.val(Math.min(Math.max(num_cores.val(), 1), cmax))
     }
 }
 
-function _update_nodes_inputs(selected_custom_queue) {
-    let queue_name = selected_custom_queue[0].value
+function _update_nodes_inputs(selected_node_type) {
+    let node_type = selected_node_type[0].value
     let num_nodes = $('#batch_connect_session_context_bc_num_slots')
 
     //console.log("Queue name:", queue_name)
     //console.log("> Num nodes:", num_nodes.val())
 
-    if (max_nodes.has(queue_name)) {
-        let nmax = max_nodes.get(queue_name)
+    if (max_nodes.has(node_type)) {
+        let nmax = max_nodes.get(node_type)
         //console.log("> Max nodes: ", nmax)
         num_nodes.attr('max', nmax)
         num_nodes.val(Math.min(Math.max(num_nodes.val(), 1), nmax))
     }
 }
 
-function _update_walltime_inputs(selected_custom_queue) {
-    let queue_name = selected_custom_queue[0].value
+function _update_walltime_inputs(selected_node_type) {
+    let node_type = selected_node_type[0].value
     let num_hours = $('#batch_connect_session_context_bc_num_hours')
 
     //console.log("Queue name:", queue_name)
     //console.log("> Num hours:", num_hours.val())
 
-    if (max_walltimes.has(queue_name)) {
-        let wmax = max_walltimes.get(queue_name)
+    if (max_walltimes.has(node_type)) {
+        let wmax = max_walltimes.get(node_type)
         //console.log("> Max hours: ", wmax)
         num_hours.attr('max', wmax)
         num_hours.val(Math.min(Math.max(num_hours.val(), 1), wmax))
     }
 }
 
-function custom_queue_change_handler(selected_custom_queue) {
-    _update_gpu_inputs(selected_custom_queue)
-    _update_cores_inputs(selected_custom_queue)
-    _update_nodes_inputs(selected_custom_queue)
-    _update_walltime_inputs(selected_custom_queue)
+function node_type_change_handler(selected_node_type) {
+    _update_gpu_inputs(selected_node_type)
+    _update_cores_inputs(selected_node_type)
+    _update_nodes_inputs(selected_node_type)
+    _update_walltime_inputs(selected_node_type)
 }
 
 $(document).ready(function () {
-    let custom_queue = $('#batch_connect_session_context_custom_queue')
+    let node_type = $('#batch_connect_session_context_node_type')
   
     // Initialize everything on page load
-   custom_queue_change_handler(custom_queue)
+   node_type_change_handler(node_type)
   
     //Handles the change events
-    custom_queue.change(function () { custom_queue_change_handler(custom_queue) })
+    node_type.change(function () { node_type_change_handler(node_type) })
   })
--- get_resource_limits.py
+++ get_resource_limits.py
@@ -11,10 +11,26 @@ import re
 js_output_file="resource_limits.js"
 
 # Don't get limits for these partitions
-excluded_parts = {"phoenix-all"}
+excluded_parts = {
+    "phoenix-all",
+    "cpu-medium-X",
+    "cpu-large-X",
+    "cpu-sas-X",
+    "cpu-amd-X",
+    "gpu-v100-X",
+    "gpu-a100-X",
+    "gpu-rtx6000-X",
+    "cpu-pmem-X"
+}
+
+# Maps a node type to the actual feature
+ntype_to_feat = {
+    "cpu": "cpu-small",
+    "V100-16GB": "V100-16GB"
+}
 
 # Read data table from sinfo
-proc = subprocess.run('sinfo -o "%P %D %l %c %m %G"', shell=True, check=True, 
+proc = subprocess.run('sinfo -o "%P %f %D %l %c %m %G"', shell=True, check=True, 
         capture_output=True, text=True)
 limit_data = csv.DictReader(proc.stdout.rstrip().split('\n'), delimiter=' ')
 
@@ -22,39 +38,46 @@ limit_data = csv.DictReader(proc.stdout.rstrip().split('\n'), delimiter=' ')
 # Value is a list of [paritition, limit_value] pairs
 limit_vars = defaultdict(list)
 
+# limit_data must be outermost loop, since DictReader can only
+# be iterated over one time.
 for row in limit_data:
-    p = row["PARTITION"].rstrip('*')
-    if p in excluded_parts:
-        continue
+    part = row["PARTITION"]
+    avail_feats = row["AVAIL_FEATURES"].split(",")
+    print(f"{part}: {avail_feats}")
+    for ntype, feat in ntype_to_feat.items():
+        print(f"     {ntype}: {feat}")
+        if part in excluded_parts or feat not in avail_feats:
+            print("        skipping")
+            continue
 
-    # Max Nodes
-    n = int(row["NODES"])
-    limit_vars['max_nodes'].append([p, n])
+        # Max Nodes
+        n = int(row["NODES"])
+        limit_vars['max_nodes'].append([ntype, n])
 
-    # Max walltime
-    t = row["TIMELIMIT"]
-    # sinfo doesn't output an ISO format, so parse it manually
-    if "-" in t:
-        days, t = t.split('-')
-    else:
-        days = 0
-    total = int(days) * 24 + int(t.split(':')[0])
-    limit_vars["max_walltimes"].append([p, total])
+        # Max walltime
+        t = row["TIMELIMIT"]
+        # sinfo doesn't output an ISO format, so parse it manually
+        if "-" in t:
+            days, t = t.split('-')
+        else:
+            days = 0
+        total = int(days) * 24 + int(t.split(':')[0])
+        limit_vars["max_walltimes"].append([ntype, total])
 
-    # Max cores
-    c = int(row["CPUS"])
-    limit_vars["max_cores"].append([p, c])
+        # Max cores
+        c = int(row["CPUS"])
+        limit_vars["max_cores"].append([ntype, c])
 
-    # Max memory
-    m = int(row["MEMORY"]) // 1000
-    limit_vars["max_node_mem"].append([p, m])
+        # Max memory
+        m = int(row["MEMORY"]) // 1000
+        limit_vars["max_node_mem"].append([ntype,m])
 
-    # Max GPUs
-    g = 0
-    match = re.search(r"^gpu:\w+:(\d+)", row["GRES"])
-    if match:
-        g = int(match.group(1))
-    limit_vars["max_gpus"].append([p, g])
+        # Max GPUs
+        g = 0
+        match = re.search(r"^gpu:\w+:(\d+)", row["GRES"])
+        if match:
+            g = int(match.group(1))
+        limit_vars["max_gpus"].append([ntype, g])
 
 # Write to .js header file. 
 # Formatting to make it easier for humans to read
