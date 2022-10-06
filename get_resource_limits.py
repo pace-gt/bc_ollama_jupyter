87c6002fad4d072b67060208053682ecb2db7e45
Ron Rahaman <rrahaman6@gatech.edu>
2022-10-06 16:26:23 -0400

    RR: Fixed get_resource_limits for phoenix prod

--- get_resource_limits.py
+++ get_resource_limits.py
@@ -6,6 +6,7 @@ import subprocess
 from collections import defaultdict
 import csv
 import re
+from typing import NamedTuple
 
 # Output to this JavaScript header file
 js_output_file="resource_limits.js"
@@ -30,30 +31,40 @@ ntype_to_feat = {
     "V100-32GB": "V100-32GB"
 }
 
-# Read data table from sinfo
-proc = subprocess.run('sinfo -o "%P %f %D %l %c %m %G"', shell=True, check=True, 
-        capture_output=True, text=True)
-limit_data = csv.DictReader(proc.stdout.rstrip().split('\n'), delimiter=' ')
+
+class NodeTypeInfo(NamedTuple):
+    node_type: str
+    part_name: str
+    inc_feat: set = {}
+
+node_types = [
+    NodeTypeInfo(node_type="cpu", part_name="cpu-small"),
+    NodeTypeInfo(node_type="cpusas", part_name="cpu-sas"),
+    NodeTypeInfo(node_type="V100-16GB", part_name="gpu-v100", inc_feat={"V100-16GB"}),
+    NodeTypeInfo(node_type="V100-32GB", part_name="gpu-v100", inc_feat={"V100-32GB"}),
+    NodeTypeInfo(node_type="RTX6000", part_name="gpu-rtx6000")
+]
 
 # Key is variable name for a resource limit ('max_nodes', etc).  
 # Value is a list of [paritition, limit_value] pairs
 limit_vars = defaultdict(list)
 
-# limit_data must be outermost loop, since DictReader can only
-# be iterated over one time.
-for row in limit_data:
-    part = row["PARTITION"]
-    avail_feats = row["AVAIL_FEATURES"].split(",")
-    print(f"{part}: {avail_feats}")
-    for ntype, feat in ntype_to_feat.items():
-        print(f"     {ntype}: {feat}")
-        if part in excluded_parts or feat not in avail_feats:
-            print("        skipping")
-            continue
+for nt in node_types:
+    if nt.inc_feat:
+        cmd = f'sinfo -o "%D %l %c %m %G %f" -p {nt.part_name}'
+    else:
+        cmd = f'sinfo -o "%D %l %c %m %G" -p {nt.part_name}'
+    proc = subprocess.run(cmd, shell=True, check=True, capture_output=True, text=True)
+    limit_data = csv.DictReader(proc.stdout.rstrip().split('\n'), delimiter=' ')
+    for row in limit_data:
+        if nt.inc_feat:
+            avail_feat = set(row["AVAIL_FEATURES"].split(","))
+            if not nt.inc_feat.issubset(avail_feat):
+                continue
 
         # Max Nodes
         n = int(row["NODES"])
-        limit_vars['max_nodes'].append([ntype, n])
+        limit_vars['max_nodes'].append([nt.node_type, n])
 
         # Max walltime
         t = row["TIMELIMIT"]
@@ -63,22 +74,22 @@ for row in limit_data:
         else:
             days = 0
         total = int(days) * 24 + int(t.split(':')[0])
-        limit_vars["max_walltimes"].append([ntype, total])
+        limit_vars["max_walltimes"].append([nt.node_type, total])
 
         # Max cores
         c = int(row["CPUS"])
-        limit_vars["max_cores"].append([ntype, c])
+        limit_vars["max_cores"].append([nt.node_type, c])
 
         # Max memory
-        m = int(row["MEMORY"]) // 1000
-        limit_vars["max_node_mem"].append([ntype,m])
+        m = int(row["MEMORY"].rstrip('+')) // 1000
+        limit_vars["max_node_mem"].append([nt.node_type,m])
 
         # Max GPUs
         g = 0
         match = re.search(r"^gpu:\w+:(\d+)", row["GRES"])
         if match:
             g = int(match.group(1))
-        limit_vars["max_gpus"].append([ntype, g])
+        limit_vars["max_gpus"].append([nt.node_type, g])
 
 # Write to .js header file. 
 # Formatting to make it easier for humans to read
